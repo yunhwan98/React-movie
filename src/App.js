93e8413618c0b4563ec7d09f420e4a1ea2c1c718
yunhwan98 <79241793+yunhwan98@users.noreply.github.com>
2022-09-14 21:02:33 +0900

    첫 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [counter, setValue] =useState(0);
+  const [keyword, setKeyword] =useState("");
+  const onChange = (event) =>setKeyword(event.target.value);
+  const onClick = () =>  setValue((prev)=>prev+1);
+
+
+  useEffect(()=>{
+    console.log('I run only once');
+  },[]);
+
+  useEffect(()=>{
+    console.log("I run when 'keyword' changes");
+  },[keyword]);
+
+  useEffect(()=>{
+    console.log("I run when 'counter' changes");
+  },[counter]);
+
+  useEffect(()=>{
+    console.log("I run when keyboard &counter changes");
+  },[keyword,counter]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value = {keyword} onChange={onChange} type="text" placeholder="Serach here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+
+function Button({text}){
+    return <button style={{
+        backgroundColor :"tomato",
+        color: "white"
+
+    }}>{text}</button>
+}
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+};
+
+export default Button;
